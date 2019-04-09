62f1ebc88cad672a5963375ef697fd05153b6eb7
Galen Vincent <galenvincent@mymail.mines.edu>
2019-04-08 23:13:04 -0600

    Fix on the addition of foreign keys

--- data_import.sql
+++ data_import.sql
@@ -56,6 +56,6 @@ weather_delay INTEGER
 \COPY airport FROM 'airports.csv' WITH CSV HEADER;
 \COPY flights (year, month, day, day_of_week, airline_id, flight_no, tail_no, origin_airport_id, dest_airport_id, scheduled_dep_time, dep_time, dep_delay, taxi_out_time, wheels_off_time, scheduled_time, elapsed_time, air_time, distance, wheels_on_time, taxi_in_time, scheduled_arr_time, arr_time, arr_delay, diverted, cancelled, cancel_reason, air_system_delay, security_delay, airline_delay, late_aircraft_delay, weather_delay) FROM 'flights.csv' WITH CSV HEADER;
 
-ALTER TABLE flights ADD CONSTRAINT constraint_fkey FOREIGN KEY (airline_id) REFERENCES airline (id);
-ALTER TABLE flights ADD CONSTRAINT constraint_fkey FOREIGN KEY (origin_airport_id) REFERENCES airport (id);
-ALTER TABLE flights ADD CONSTRAINT constraint_fkey FOREIGN KEY (dest_airport_id) REFERENCES airport (id);
+ALTER TABLE flights ADD CONSTRAINT constraint_fkey1 FOREIGN KEY (airline_id) REFERENCES airline (id);
+ALTER TABLE flights ADD CONSTRAINT constraint_fkey2 FOREIGN KEY (origin_airport_id) REFERENCES airport (id);
+ALTER TABLE flights ADD CONSTRAINT constraint_fkey3 FOREIGN KEY (dest_airport_id) REFERENCES airport (id);
